97687cfc0fa8aa08fa15fb9a8f31fc74d8042947
Ceylin <caltepeceylin@gmail.com>
2023-07-31 02:02:48 +0300

    feat: create-to-do-list-item

---
+++ todo/src/components/TodoList.tsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+
+function TodoList() {
+  return (
+    <ul className="App">
+
+    </ul>
+  );
+}
+
+export default TodoList;
---
+++ todo/src/components/list-item/ListItem.tsx
@@ -0,0 +1,33 @@
+import React, { useState } from 'react';
+import './ListItem.css';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { faTrashCan, faPenToSquare } from '@fortawesome/free-regular-svg-icons';
+import { faCheck } from '@fortawesome/free-solid-svg-icons';
+
+function ListItem(todoitem : {mission: string, date: string, isDone: boolean}) {
+  const[todo, setTodo] = useState(todoitem);
+  const[isUpdate, setUpdate] = useState(false);
+
+  return (
+    <li className="list-item">
+      <input type="checkbox" checked={todo.isDone} onChange={() => setTodo({...todo, isDone: !todo.isDone})}/>
+      <FontAwesomeIcon className='checkbox-icon' icon={faCheck} />
+    {
+    isUpdate ?
+      <div className='text-update'>
+        <input type="text" value={todo.mission} onChange={(event) => (setTodo({...todo, mission: event.target.value}))} />
+        <div className="save-button" onClick={() => (setUpdate(false))}>Save</div>
+      </div>
+      :
+      <div className="list-item-text">{todo.mission}</div>
+    }
+    <div className="list-item-date">{todo.date}</div>
+    <ul className="list-item-menu">
+        <li className="menu-item"><FontAwesomeIcon icon={faTrashCan} /></li>
+        <li className="menu-item" onClick={() => setUpdate(true)}><FontAwesomeIcon icon={faPenToSquare} /></li>
+    </ul>
+    </li>
+  );
+}
+
+export default ListItem;
